a8f1ddc12c75eb3d0509a3167a398bfdd67be987
sergio <sergioatamorim@gmail.com>
2017-05-12 02:41:09 -0300

    using DAO to manipulate objects

--- SE/src/main/java/br/com/tpartner/se/CSVReader.java
+++ SE/src/main/java/br/com/tpartner/se/CSVReader.java
@@ -5,7 +5,9 @@
  */
 package br.com.tpartner.se;
 
-import br.com.tpartner.persistence.crud.implementation.StudentCRUDImplementation;
+import br.com.tpartner.persistence.crud.AccessSessionCRUD;
+import br.com.tpartner.persistence.crud.ActionCRUD;
+import br.com.tpartner.persistence.crud.StudentCRUD;
 import br.com.tpartner.persistence.model.AccessSession;
 import br.com.tpartner.persistence.model.Action;
 import br.com.tpartner.persistence.model.Student;
@@ -21,6 +23,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -32,13 +35,22 @@ public class CSVReader {
         CSVReader reader;
         reader = new CSVReader();
         List<List<String>> csvMatrix;
-        csvMatrix = reader.getMatrix("C:\\Users\\sergi\\Downloads\\logs.csv", ";", "iso-8859-1");
-        reader.createStudents(csvMatrix);
-        reader.createSessionsA(csvMatrix);
-        reader.createActions(csvMatrix);
+        csvMatrix = reader.getMatrix("/home/sergio/Documents/logs.csv", ";", "iso-8859-1");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        reader.run(context, csvMatrix);
+        context.close();
         System.out.println("OK");
     }
-
+    
+    private void run(ClassPathXmlApplicationContext context, List<List<String>> csvMatrix) throws ParseException {
+        StudentCRUD studentDAO = context.getBean(StudentCRUD.class);
+        this.createStudents(csvMatrix, studentDAO);
+        AccessSessionCRUD accessSessionDAO = context.getBean(AccessSessionCRUD.class);
+        this.createAccessSessions(csvMatrix, accessSessionDAO, studentDAO);
+        ActionCRUD actionDAO = context.getBean(ActionCRUD.class);
+        this.createActions(csvMatrix, actionDAO, studentDAO, accessSessionDAO);
+    }
+    
     private SimpleDateFormat getTimestampType(String timeString){
         SimpleDateFormat dateFormat;
         if (timeString.charAt(2) == '/'){
@@ -68,50 +80,49 @@ public class CSVReader {
         return action;
     }
     
-    private Student getStudent(List<String> line) {
-        StudentCRUDImplementation studentCRUDImplementation = new StudentCRUDImplementation();
+    private Student getStudent(List<String> line, StudentCRUD studentDAO) {
         Student student;
-        try {
-            student = studentCRUDImplementation.findByStudentIdString(line.get(0));
-        }
-        catch (Exception e) {
+        student = studentDAO.findByIdString(line.get(0));
+        if (student == null) {
             student = new Student(line.get(0));
+            studentDAO.save(student);
+            student = studentDAO.findByIdString(line.get(0));
         }
         return student;
     }
     
-    private AccessSession getAccessSession(List<String> line) throws ParseException {
-        Student student = getStudent(line);
+    private AccessSession getAccessSession(List<String> line, StudentCRUD studentDAO) throws ParseException {
+        Student student = getStudent(line, studentDAO);
         Date dateTime = getDateTime(line);
         List<AccessSession> accessSessions = student.getAccessSessions();
         return getNearestAccessSession(accessSessions, dateTime);
     }
     
     
-    private void createStudents(List<List<String>> csvMatrix) {
+    private void createStudents(List<List<String>> csvMatrix, StudentCRUD studentDAO) {
         for (List<String> line : csvMatrix) {
-            getStudent(line);
+            getStudent(line, studentDAO);
         }
     }
     
-    private void createSessionsA(List<List<String>> csvMatrix) throws ParseException {
+    private void createAccessSessions(List<List<String>> csvMatrix, AccessSessionCRUD accessSessionDAO, StudentCRUD studentDAO) throws ParseException {
         for (List<String> line : csvMatrix) {
             if (line.get(1).contains("USER_SESSION")) {
-                Student student = getStudent(line);
-                List<AccessSession> sessionsA = student.getAccessSessions();
+                Student student = getStudent(line, studentDAO);
+                List<AccessSession> accessSessions = student.getAccessSessions();
                 AccessSession accessSession = new AccessSession(getDateTime(line));
-                if (!sessionsA.contains(accessSession)) {
-                    sessionsA.add(accessSession);
-                    student.setAccessSessions(sessionsA);
-                }
+                accessSessionDAO.save(accessSession);
+                accessSessions.add(accessSession);
+                student.setAccessSessions(accessSessions);
+                studentDAO.update(student);
             }
         }
     }
     
-    private AccessSession getNearestAccessSession(List<AccessSession> sessionsA, Date dateTime) {
+    private AccessSession getNearestAccessSession(List<AccessSession> accessSessions, Date dateTime) {
         AccessSession nearestAccessSession = null;
         Integer nearestComparation = Integer.MAX_VALUE;
-        for (AccessSession accessSession : sessionsA) {
+        for (AccessSession accessSession : accessSessions) {
             if (accessSession.getTimeStart().compareTo(dateTime) >= 0) {
                 if (accessSession.getTimeStart().compareTo(dateTime) < nearestComparation) {
                     nearestComparation = accessSession.getTimeStart().compareTo(dateTime);
@@ -122,16 +133,16 @@ public class CSVReader {
         return nearestAccessSession;
     }
     
-    private void createActions(List<List<String>> csvMatrix) throws ParseException{
+    private void createActions(List<List<String>> csvMatrix, ActionCRUD actionDAO, StudentCRUD studentDAO, AccessSessionCRUD accessSessionDAO) throws ParseException{
         for (List<String> line : csvMatrix) {
             if (!line.get(1).contains("USER_SESSION")&&!line.get(0).contains("STUDENT_ID")) {
-                AccessSession accessSession = getAccessSession(line);
+                AccessSession accessSession = getAccessSession(line, studentDAO);
                 Action action = getAction(line);
-                if (!accessSession.getActions().contains(action)) {
-                    List<Action> actions = accessSession.getActions();
-                    actions.add(action);
-                    accessSession.setActions(actions);
-                }
+                actionDAO.save(action);
+                List<Action> actions = accessSession.getActions();
+                actions.add(action);
+                accessSession.setActions(actions);
+                accessSessionDAO.update(accessSession);
             }
         }
     }
