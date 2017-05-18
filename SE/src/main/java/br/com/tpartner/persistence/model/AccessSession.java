3972b2dd19099cf6ebba985e95a9a7d92ea598bd
sergio <sergioatamorim@gmail.com>
2017-05-18 16:42:29 -0300

    subSessions list removed from AccessSession class

--- SE/src/main/java/br/com/tpartner/persistence/model/AccessSession.java
+++ SE/src/main/java/br/com/tpartner/persistence/model/AccessSession.java
@@ -7,8 +7,6 @@ package br.com.tpartner.persistence.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -37,9 +34,6 @@ public class AccessSession implements Serializable {
     @JoinColumn(name = "student_id", referencedColumnName = "id")
     @ManyToOne
     private Student student;
-    
-    @OneToMany(mappedBy = "accessSession", cascade = CascadeType.ALL)
-    private List<SubSession> subSessions;
     
     @Temporal(javax.persistence.TemporalType.TIMESTAMP)
     @Column(name = "time_start")
