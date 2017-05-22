ddd6469cf4a45b5082732c04546247e4f6f05e71
sergio <sata@ic.ufal.br>
2017-05-22 15:20:37 -0300

    time-filter removed from app.js config file

--- TP/js/app.js
+++ TP/js/app.js
@@ -7,33 +7,3 @@ app.config(['$routeProvider', function ($routeProvider) {
         templateUrl: 'view/summary.html'
     }).otherwise('/summary');
 }]);
-
-app.filter('millSecondsToTimeString', function() {
-  return function(millseconds) {
-    var oneSecond = 1000;
-    var oneMinute = oneSecond * 60;
-    var oneHour = oneMinute * 60;
-    var oneDay = oneHour * 24;
-
-    var seconds = Math.floor((millseconds % oneMinute) / oneSecond);
-    var minutes = Math.floor((millseconds % oneHour) / oneMinute);
-    var hours = Math.floor((millseconds % oneDay) / oneHour);
-    var days = Math.floor(millseconds / oneDay);
-
-    var timeString = '';
-    if (days !== 0) {
-        timeString += (days !== 1) ? (days + ' days ') : (days + ' day ');
-    }
-    if (hours !== 0) {
-        timeString += (hours !== 1) ? (hours + ' hours ') : (hours + ' hour ');
-    }
-    if (minutes !== 0) {
-        timeString += (minutes !== 1) ? (minutes + ' minutes ') : (minutes + ' minute ');
-    }
-    if (seconds !== 0 || millseconds < 1000) {
-        timeString += (seconds !== 1) ? (seconds + ' seconds ') : (seconds + ' second ');
-    }
-
-    return timeString;
-};
-});
