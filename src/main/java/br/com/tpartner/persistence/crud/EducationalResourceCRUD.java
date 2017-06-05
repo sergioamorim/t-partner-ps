9bbfb8575d64df338f3c8b1317fe482d9d26559f
sergio <sergioatamorim@gmail.com>
2017-06-05 16:14:33 -0300

    EducationalResourceCRUD added

---
+++ src/main/java/br/com/tpartner/persistence/crud/EducationalResourceCRUD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tpartner.persistence.crud;
+
+import br.com.tpartner.persistence.model.EducationalResource;
+import java.util.List;
+
+/**
+ *
+ * @author sergio
+ */
+public interface EducationalResourceCRUD {
+    /* Client */
+    public EducationalResource save(EducationalResource educationalResource);
+    public EducationalResource update(EducationalResource educationalResource);
+    public void delete(EducationalResource educationalResource);
+    /* Aux */
+    public EducationalResource findById(String educationalResourceId);
+    public List<EducationalResource> findAll();
+}
