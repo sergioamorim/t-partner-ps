b71f3667b9186c2ba4c0037fd64597cfef50ffbe
sergio <sata@ic.ufal.br>
2017-06-05 19:00:00 -0300

    NonMappedStudentActionCRUD interface created

---
+++ src/main/java/br/com/tpartner/persistence/crud/NonMappedStudentActionCRUD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tpartner.persistence.crud;
+
+import br.com.tpartner.persistence.model.NonMappedStudentAction;
+import java.util.List;
+
+/**
+ *
+ * @author sergio
+ */
+public interface NonMappedStudentActionCRUD {
+    /* Client */
+    public NonMappedStudentAction save(NonMappedStudentAction nonMappedStudentAction);
+    public NonMappedStudentAction update(NonMappedStudentAction nonMappedStudentAction);
+    public void delete(NonMappedStudentAction nonMappedStudentAction);
+    /* Aux */
+    public NonMappedStudentAction findById(String nonMappedStudentActionId);
+    public List<NonMappedStudentAction> findAll();
+}
