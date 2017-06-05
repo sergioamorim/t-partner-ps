ad8a2e7f2f430193991b26d699991800807a8fc9
sergio <sata@ic.ufal.br>
2017-06-05 19:00:33 -0300

    ProblemSolvingCRUD interface created to ProblemSolving persistence

---
+++ src/main/java/br/com/tpartner/persistence/crud/ProblemSolvingCRUD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tpartner.persistence.crud;
+
+import br.com.tpartner.persistence.model.ProblemSolving;
+import java.util.List;
+
+/**
+ *
+ * @author sergio
+ */
+public interface ProblemSolvingCRUD {
+    /* Client */
+    public ProblemSolving save(ProblemSolving problemSolving);
+    public ProblemSolving update(ProblemSolving problemSolving);
+    public void delete(ProblemSolving problemSolving);
+    /* Aux */
+    public ProblemSolving findById(String problemSolvingId);
+    public List<ProblemSolving> findAll();
+}
