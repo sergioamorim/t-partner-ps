264309d6783a04f330b58b1896f78583229dd2f8
sergio <sata@ic.ufal.br>
2017-06-05 09:16:22 -0300

    modifications on MultipleChoiceProblem class

--- src/main/java/br/com/tpartner/persistence/model/MultipleChoiceProblem.java
+++ src/main/java/br/com/tpartner/persistence/model/MultipleChoiceProblem.java
@@ -5,35 +5,19 @@
  */
 package br.com.tpartner.persistence.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author sergio
  */
 public class MultipleChoiceProblem {
     private final String id;
-    private final List<Integer> responseTimes;
     
-    public MultipleChoiceProblem(String id, Integer responseTime) {
+    public MultipleChoiceProblem(String id) {
         this.id = id;
-        this.responseTimes = new ArrayList<Integer>();
-        this.responseTimes.add(responseTime);
     }
     
     public String getId() {
         return this.id;
     }
 
-    public Integer getResponseTimeAverage() {
-        Integer totalResponseTime;
-        totalResponseTime = this.responseTimes.stream().mapToInt(Integer::intValue).sum();
-        return totalResponseTime;
-    }
-
-    public void addResponseTime(Integer responseTime) {
-        this.responseTimes.add(responseTime);
-    }
-    
 }
