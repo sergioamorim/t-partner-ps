ca4cc6dfdb5647653ee4a184b9b46ff2c7f31695
sergio <sata@ic.ufal.br>
2017-06-07 09:12:44 -0300

    ProblemSolvingFacade interface created for web services management

---
+++ src/main/java/br/com/tpartner/services/facade/ProblemSolvingFacade.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tpartner.services.facade;
+
+import br.com.tpartner.persistence.model.ProblemSolving;
+import java.util.List;
+
+/**
+ *
+ * @author sergio
+ */
+public interface ProblemSolvingFacade {
+    /* Client*/
+    public ProblemSolving save(ProblemSolving problemSolving);
+    public ProblemSolving update(ProblemSolving problemSolving);
+    public void delete(ProblemSolving problemSolving);
+    /* Aux */
+    public ProblemSolving findById(Integer problemSolvingId);
+    public List<ProblemSolving> findAll();
+}
