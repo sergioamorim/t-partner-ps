34331c2abcf82773efa62eee1c303e914ca638b1
sergio <sata@ic.ufal.br>
2017-06-08 11:47:09 -0300

    major update to GamingTheSystem class

--- src/main/java/br/com/tpartner/persistence/model/GamingTheSystem.java
+++ src/main/java/br/com/tpartner/persistence/model/GamingTheSystem.java
@@ -5,10 +5,48 @@
  */
 package br.com.tpartner.persistence.model;
 
+import br.com.tpartner.persistence.crud.EducationalResourceCRUD;
+import br.com.tpartner.persistence.crud.ResourceInteractionCRUD;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  *
  * @author sergio
  */
 public class GamingTheSystem {
+    private List<EducationalResource> educationalResources;
+    private List<EducationalResourceStats> educationalResourcesStats;
     
+    private GamingTheSystem() {
+        
+        AbstractApplicationContext context;
+        context = new ClassPathXmlApplicationContext("spring.xml");
+        
+        EducationalResourceCRUD educationalResourceDAO;
+        educationalResourceDAO = context.getBean(EducationalResourceCRUD.class);
+        
+        ResourceInteractionCRUD resourceInteractionDAO;
+        resourceInteractionDAO = context.getBean(ResourceInteractionCRUD.class);
+        
+        educationalResources = educationalResourceDAO.findAll();
+        
+        educationalResourcesStats = new ArrayList<EducationalResourceStats>();
+        getStats(educationalResources, educationalResourcesStats,
+                resourceInteractionDAO);
+        
+    }
+ 
+    private void getStats(
+            List<EducationalResource> educationalResources,
+            List<EducationalResourceStats> educationalResourcesStats,
+            ResourceInteractionCRUD resourceInteractionDAO) {
+        if (!educationalResources.isEmpty()) {
+            educationalResourcesStats.add(new EducationalResourceStats(
+                    educationalResources.remove(0), resourceInteractionDAO));
+            getStats(educationalResources, educationalResourcesStats,
+                    resourceInteractionDAO);
+        }
+    }
 }
