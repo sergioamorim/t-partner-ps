5795c18e27c173b32c44d9501442886255c88fae
sergio <sergioatamorim@gmail.com>
2017-06-15 10:52:45 -0300

    ResourceInteractionCRUDImplementation now extends GenericCRUDImplementation

--- src/main/java/br/com/tpartner/persistence/crud/implementation/ResourceInteractionCRUDImplementation.java
+++ src/main/java/br/com/tpartner/persistence/crud/implementation/ResourceInteractionCRUDImplementation.java
@@ -11,10 +11,7 @@ import br.com.tpartner.persistence.model.ResourceInteraction;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -23,72 +20,8 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class ResourceInteractionCRUDImplementation
+        extends GenericCRUDImplementation<ResourceInteraction, Integer>
         implements ResourceInteractionCRUD {
-    
-    @Autowired
-    private SessionFactory sessionFactory;
-    public void setSessionFactory (SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    public Session getCurrentSession() {
-        return sessionFactory.openSession();
-    }
-    
-    @Override
-    public ResourceInteraction save(ResourceInteraction resourceInteraction) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(resourceInteraction);
-        tx.commit();
-        session.close();
-        return resourceInteraction;
-    }
-    
-    @Override
-    public ResourceInteraction update(ResourceInteraction resourceInteraction) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(resourceInteraction);
-        tx.commit();
-        session.close();
-        return resourceInteraction;
-    }
-    
-    @Override
-    public void delete(ResourceInteraction resourceInteraction) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.delete(resourceInteraction);
-        tx.commit();
-        session.close();
-    }
-
-    @Override
-    public ResourceInteraction findById(Integer resourceInteractionId) {
-        Session session = getCurrentSession();
-        Criteria createCriteria;
-        createCriteria = session.createCriteria(ResourceInteraction.class);
-        createCriteria.add(Restrictions.eq("id", resourceInteractionId));
-        
-        ResourceInteraction resourceInteraction;
-        resourceInteraction = (ResourceInteraction)
-                createCriteria.uniqueResult();
-        
-        session.close();
-        return resourceInteraction;
-    }
-
-    @Override
-    public List<ResourceInteraction> findAll() {
-        Session session = getCurrentSession();
-        
-        List<ResourceInteraction> resourceInteractions;        
-        resourceInteractions = session.createCriteria(
-                ResourceInteraction.class).list();
-        
-        session.close();
-        return resourceInteractions;
-    }
     
     @Override
     public List<ResourceInteraction> findByEducationalResource(
@@ -105,4 +38,5 @@ public class ResourceInteractionCRUDImplementation
         session.close();
         return resourceInteractions;
     }
+    
 }
