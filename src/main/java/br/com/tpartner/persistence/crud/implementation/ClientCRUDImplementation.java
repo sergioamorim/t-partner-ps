406027ef07fe0def67c2a1218610df183544ee66
sergio <sergioatamorim@gmail.com>
2017-06-15 10:43:09 -0300

    ClientCRUDImplementation now extends GenericCRUDImplementation

--- src/main/java/br/com/tpartner/persistence/crud/implementation/ClientCRUDImplementation.java
+++ src/main/java/br/com/tpartner/persistence/crud/implementation/ClientCRUDImplementation.java
@@ -7,13 +7,6 @@ package br.com.tpartner.persistence.crud.implementation;
 
 import br.com.tpartner.persistence.crud.ClientCRUD;
 import br.com.tpartner.persistence.model.Client;
-import java.util.List;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -21,60 +14,8 @@ import org.springframework.stereotype.Repository;
  * @author sergio
  */
 @Repository
-public class ClientCRUDImplementation implements ClientCRUD {
-    @Autowired
-    private SessionFactory sessionFactory;
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    public Session getCurrentSession() {
-        return sessionFactory.openSession();
-    }
-
-    @Override
-    public Client save(Client client) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(client);
-        tx.commit();
-        session.close();
-        return client;
-    }
-
-    @Override
-    public Client update(Client client) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(client);
-        tx.commit();
-        session.close();
-        return client;
-    }
-
-    @Override
-    public void delete(Client client) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.delete(client);
-        tx.commit();
-        session.close();
-    }
-
-    @Override
-    public List<Client> findAll() {
-        Session session = getCurrentSession();
-        List<Client> listClient = session.createCriteria(Client.class).list();
-        session.close();
-        return listClient;
-    }
-
-    @Override
-    public Client findById(Integer clientId) {
-        Session session = getCurrentSession();
-        Criteria createCriteria = session.createCriteria(Client.class);
-        createCriteria.add(Restrictions.eq("id", clientId));
-        Client c = (Client) createCriteria.uniqueResult();
-        session.close();
-        return c;
-    }
+public class ClientCRUDImplementation 
+        extends GenericCRUDImplementation<Client, Integer>
+        implements ClientCRUD {
+    
 }
