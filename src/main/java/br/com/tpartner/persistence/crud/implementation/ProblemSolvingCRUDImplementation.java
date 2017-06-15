41ae74931729356f7b352478844c8be22909da36
sergio <sergioatamorim@gmail.com>
2017-06-15 10:50:19 -0300

    ProblemSolvingCRUDImplementation now extends GenericCRUDImplementation

--- src/main/java/br/com/tpartner/persistence/crud/implementation/ProblemSolvingCRUDImplementation.java
+++ src/main/java/br/com/tpartner/persistence/crud/implementation/ProblemSolvingCRUDImplementation.java
@@ -7,13 +7,6 @@ package br.com.tpartner.persistence.crud.implementation;
 
 import br.com.tpartner.persistence.crud.ProblemSolvingCRUD;
 import br.com.tpartner.persistence.model.ProblemSolving;
-import java.util.List;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -21,60 +14,8 @@ import org.springframework.stereotype.Repository;
  * @author sergio
  */
 @Repository
-public class ProblemSolvingCRUDImplementation implements ProblemSolvingCRUD {
-    @Autowired
-    private SessionFactory sessionFactory;
-    public void setSessionFactory (SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    public Session getCurrentSession() {
-        return sessionFactory.openSession();
-    }
+public class ProblemSolvingCRUDImplementation 
+        extends GenericCRUDImplementation<ProblemSolving, Integer>
+        implements ProblemSolvingCRUD {
     
-    @Override
-    public ProblemSolving save(ProblemSolving problemSolving) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(problemSolving);
-        tx.commit();
-        session.close();
-        return problemSolving;
-    }
-    
-    @Override
-    public ProblemSolving update(ProblemSolving problemSolving) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(problemSolving);
-        tx.commit();
-        session.close();
-        return problemSolving;
-    }
-    
-    @Override
-    public void delete(ProblemSolving problemSolving) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.delete(problemSolving);
-        tx.commit();
-        session.close();
-    }
-
-    @Override
-    public ProblemSolving findById(Integer problemSolvingId) {
-        Session session = getCurrentSession();
-        Criteria createCriteria = session.createCriteria(ProblemSolving.class);
-        createCriteria.add(Restrictions.eq("id", problemSolvingId));
-        ProblemSolving problemSolving = (ProblemSolving) createCriteria.uniqueResult();
-        session.close();
-        return problemSolving;
-    }
-
-    @Override
-    public List<ProblemSolving> findAll() {
-        Session session = getCurrentSession();
-        List<ProblemSolving> problemSolvings = session.createCriteria(ProblemSolving.class).list();
-        session.close();
-        return problemSolvings;
-    }
 }
