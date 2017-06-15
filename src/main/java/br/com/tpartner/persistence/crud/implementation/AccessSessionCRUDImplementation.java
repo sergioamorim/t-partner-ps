57bc9d5cde94f251df755257eb247e866fbfbc6c
sergio <sergioatamorim@gmail.com>
2017-06-15 10:42:10 -0300

    AccessSessionCRUDImplementation now extends GenericCRUDImplementation

--- src/main/java/br/com/tpartner/persistence/crud/implementation/AccessSessionCRUDImplementation.java
+++ src/main/java/br/com/tpartner/persistence/crud/implementation/AccessSessionCRUDImplementation.java
@@ -11,10 +11,7 @@ import br.com.tpartner.persistence.model.Student;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -22,54 +19,9 @@ import org.springframework.stereotype.Repository;
  * @author sergio
  */
 @Repository
-public class AccessSessionCRUDImplementation implements AccessSessionCRUD {
-    @Autowired
-    private SessionFactory sessionFactory;
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    public Session getCurrentSession() {
-        return sessionFactory.openSession();
-    }
-    
-    @Override
-    public AccessSession save(AccessSession accessSession) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(accessSession);
-        tx.commit();
-        session.close();
-        return accessSession;
-    }
-    
-    @Override
-    public AccessSession update(AccessSession accessSession) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(accessSession);
-        tx.commit();
-        session.close();
-        return accessSession;
-    }
-    
-    @Override
-    public void delete(AccessSession accessSession) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.delete(accessSession);
-        tx.commit();
-        session.close();
-    }
-
-    @Override
-    public AccessSession findById(Integer accessSessionId) {
-        Session session = getCurrentSession();
-        Criteria createCriteria = session.createCriteria(AccessSession.class);
-        createCriteria.add(Restrictions.eq("id", accessSessionId));
-        AccessSession accessSession = (AccessSession) createCriteria.uniqueResult();
-        session.close();
-        return accessSession;
-    }
+public class AccessSessionCRUDImplementation
+        extends GenericCRUDImplementation<AccessSession, Integer>
+        implements AccessSessionCRUD {
     
     @Override
     public List<AccessSession> findByStudent(Student student) {
@@ -80,12 +32,5 @@ public class AccessSessionCRUDImplementation implements AccessSessionCRUD {
         session.close();
         return accessSessions;
     }
-
-    @Override
-    public List<AccessSession> findAll() {
-        Session session = getCurrentSession();
-        List<AccessSession> accessSessions = session.createCriteria(AccessSession.class).list();
-        session.close();
-        return accessSessions;
-    }
+    
 }
