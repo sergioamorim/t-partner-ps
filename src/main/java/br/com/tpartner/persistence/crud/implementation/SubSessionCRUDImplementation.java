2bab3f2aecc1ac27a0c17e74dc02eaddd5309fb5
sergio <sergioatamorim@gmail.com>
2017-06-15 10:56:29 -0300

    SubSessionCRUDImplementation now extends GenericCRUDImplementation

--- src/main/java/br/com/tpartner/persistence/crud/implementation/SubSessionCRUDImplementation.java
+++ src/main/java/br/com/tpartner/persistence/crud/implementation/SubSessionCRUDImplementation.java
@@ -11,10 +11,7 @@ import br.com.tpartner.persistence.model.SubSession;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -22,54 +19,9 @@ import org.springframework.stereotype.Repository;
  * @author sergio
  */
 @Repository
-public class SubSessionCRUDImplementation implements SubSessionCRUD {
-    @Autowired
-    private SessionFactory sessionFactory;
-    public void setSessionFactory (SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    public Session getCurrentSession() {
-        return sessionFactory.openSession();
-    }
-    
-    @Override
-    public SubSession save(SubSession subSession) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(subSession);
-        tx.commit();
-        session.close();
-        return subSession;
-    }
-    
-    @Override
-    public SubSession update(SubSession subSession) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(subSession);
-        tx.commit();
-        session.close();
-        return subSession;
-    }
-    
-    @Override
-    public void delete(SubSession subSession) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.delete(subSession);
-        tx.commit();
-        session.close();
-    }
-
-    @Override
-    public SubSession findById(Integer subSessionId) {
-        Session session = getCurrentSession();
-        Criteria createCriteria = session.createCriteria(SubSession.class);
-        createCriteria.add(Restrictions.eq("id", subSessionId));
-        SubSession subSession = (SubSession) createCriteria.uniqueResult();
-        session.close();
-        return subSession;
-    }
+public class SubSessionCRUDImplementation 
+        extends GenericCRUDImplementation<SubSession, Integer>
+        implements SubSessionCRUD {
     
     @Override
     public List<SubSession> findByAccessSession(AccessSession accessSession) {
@@ -81,11 +33,4 @@ public class SubSessionCRUDImplementation implements SubSessionCRUD {
         return subSessions;
     }
 
-    @Override
-    public List<SubSession> findAll() {
-        Session session = getCurrentSession();
-        List<SubSession> subSessions = session.createCriteria(SubSession.class).list();
-        session.close();
-        return subSessions;
-    }
 }
