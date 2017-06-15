83e737ba9f002f5ac841348151be1cc33cd8e84f
sergio <sergioatamorim@gmail.com>
2017-06-15 10:48:35 -0300

    NonMappedStudentActionCRUDImplementation now extends GenericCRUDImplementation

--- src/main/java/br/com/tpartner/persistence/crud/implementation/NonMappedStudentActionCRUDImplementation.java
+++ src/main/java/br/com/tpartner/persistence/crud/implementation/NonMappedStudentActionCRUDImplementation.java
@@ -7,13 +7,6 @@ package br.com.tpartner.persistence.crud.implementation;
 
 import br.com.tpartner.persistence.crud.NonMappedStudentActionCRUD;
 import br.com.tpartner.persistence.model.NonMappedStudentAction;
-import java.util.List;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -21,61 +14,9 @@ import org.springframework.stereotype.Repository;
  * @author sergio
  */
 @Repository
-public class NonMappedStudentActionCRUDImplementation implements NonMappedStudentActionCRUD {
-    @Autowired
-    private SessionFactory sessionFactory;
-    public void setSessionFactory (SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    public Session getCurrentSession() {
-        return sessionFactory.openSession();
-    }
+public class NonMappedStudentActionCRUDImplementation 
+        extends GenericCRUDImplementation<NonMappedStudentAction, Integer>
+        implements NonMappedStudentActionCRUD {
     
-    @Override
-    public NonMappedStudentAction save(NonMappedStudentAction nonMappedStudentAction) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(nonMappedStudentAction);
-        tx.commit();
-        session.close();
-        return nonMappedStudentAction;
-    }
-    
-    @Override
-    public NonMappedStudentAction update(NonMappedStudentAction nonMappedStudentAction) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(nonMappedStudentAction);
-        tx.commit();
-        session.close();
-        return nonMappedStudentAction;
-    }
-    
-    @Override
-    public void delete(NonMappedStudentAction nonMappedStudentAction) {
-        Session session = getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.delete(nonMappedStudentAction);
-        tx.commit();
-        session.close();
-    }
-
-    @Override
-    public NonMappedStudentAction findById(Integer nonMappedStudentActionId) {
-        Session session = getCurrentSession();
-        Criteria createCriteria = session.createCriteria(NonMappedStudentAction.class);
-        createCriteria.add(Restrictions.eq("id", nonMappedStudentActionId));
-        NonMappedStudentAction nonMappedStudentAction = (NonMappedStudentAction) createCriteria.uniqueResult();
-        session.close();
-        return nonMappedStudentAction;
-    }
-
-    @Override
-    public List<NonMappedStudentAction> findAll() {
-        Session session = getCurrentSession();
-        List<NonMappedStudentAction> nonMappedStudentActions = session.createCriteria(NonMappedStudentAction.class).list();
-        session.close();
-        return nonMappedStudentActions;
-    }
 }
 
