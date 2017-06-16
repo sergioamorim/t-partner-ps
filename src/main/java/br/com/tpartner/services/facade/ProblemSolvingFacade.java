9b9f42efcfed5ca5ba258482e720c2ae3ca07a5e
sergio <sergioatamorim@gmail.com>
2017-06-16 14:41:54 -0300

    ProblemSolvingFacade interface now extends GenericFacade interface

--- src/main/java/br/com/tpartner/services/facade/ProblemSolvingFacade.java
+++ src/main/java/br/com/tpartner/services/facade/ProblemSolvingFacade.java
@@ -6,18 +6,12 @@
 package br.com.tpartner.services.facade;
 
 import br.com.tpartner.persistence.model.ProblemSolving;
-import java.util.List;
 
 /**
  *
  * @author sergio
  */
-public interface ProblemSolvingFacade {
-    /* Client*/
-    public ProblemSolving save(ProblemSolving problemSolving);
-    public ProblemSolving update(ProblemSolving problemSolving);
-    public void delete(ProblemSolving problemSolving);
-    /* Aux */
-    public ProblemSolving findById(Integer problemSolvingId);
-    public List<ProblemSolving> findAll();
+public interface ProblemSolvingFacade 
+        extends GenericFacade<ProblemSolving, Integer> {
+    
 }
